1f1d667e65cc61ceb3bfc38044a1b590c2518cc4
SultanBadri <badris846@gmail.com>
2024-06-14 13:25:46 -0700

    Update HelloController route to use /hello"

--- src/main/kotlin/com/namak/primisboot/controller/HelloController.kt
+++ src/main/kotlin/com/namak/primisboot/controller/HelloController.kt
@@ -1,10 +1,12 @@
 package com.namak.primisboot.controller
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/hello")
 class HelloController {
-    @GetMapping("/")
+    @GetMapping
     fun sayHello(): String = "Hello World!"
 }
